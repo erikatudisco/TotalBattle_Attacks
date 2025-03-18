c3eca95508225a9cc440d903d4c676522c906c37
Erika Tudisco <erika.tudisco@construcion.lth.se>
2025-03-18 13:29:43 +0100

    Fixim import bug

--- RunAttacks.py
+++ RunAttacks.py
@@ -1,19 +1,24 @@
-from Attacks import Attack
+
+import army
+import monsters
+import captains
+import Attacks
+import importlib
+importlib.reload(army)
+importlib.reload(monsters)
+importlib.reload(captains)
 from army import Army
 from monsters import Monsters
 from captains import Captains
-import importlib
-importlib.reload(Army)
-importlib.reload(Monsters)
-importlib.reload(Captains)
+from Attacks import Attack
 #### DO NOT MODIFY THE CODE ABOVE THIS LINE ####
 
 
 bonus_health = 0.72
 bonus_strength = 0.905
-monster = m.Monsters.Elf(11)
-troops = [a.Army.Spearman(3), a.Army.Rider(3), a.Army.Archers(3)]  
-captain = c.Captains.Aydae(10)
+monster = Monsters.Elf(11)
+troops = [Army.Spearman(3), Army.Rider(3), Army.Archers(3)]  
+captain = Captains.Aydae(10)
 
 
 ### DO NOT MODIFY THE CODE BELOW THIS LINE ###
